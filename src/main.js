3c368cfaef112a36968225908bca19e6a097b4a6
BaksaVrato <vratusbaksa@gmail.com>
2024-06-26 18:10:36 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+//import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
+
+createApp(App).use(router, bootstrap).mount('#app')
